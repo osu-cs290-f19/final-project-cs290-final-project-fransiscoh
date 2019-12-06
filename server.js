2b803cc28a57d7b30a6d29c3501800d355eb1766
muchgangster <jbarringer1999@gmail.com>
2019-12-06 12:25:16 -0800

    dynamic html with handlebars 404 support

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get("/", function(req, res, next) {
+    res.status(200).render("index", {});
+});
+
 app.use(express.static('public'));
+
+app.get("*", function(req, res, next) {
+    res.status(404).render("404", {
+        url: req.url
+    })
+});
 
 app.listen(port, function () {
   console.log("== Server is listening on port", port);
